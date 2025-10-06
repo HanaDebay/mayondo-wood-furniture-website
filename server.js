e7b686af6948e7bfb1001e72e8581d80d0dfd59a
Hana Debay <hana2024ug@gmail.com>
2025-10-06 09:19:31 +0300

    Fix: bind to render port

--- server.js
+++ server.js
@@ -95,4 +95,6 @@ app.use("/", managerDashboardChartRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0",() => {
+  console.log(`Server running on http://localhost:${PORT}`)
+});
